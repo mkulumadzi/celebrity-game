963b35540f4b373ba67c4d12fba685b3b5cab520
Evan Waters <evan.waters@gmail.com>
2017-05-18 11:49:46 -0400

    Added views inbetween rounds.

--- public/app/game/game-controller.js
+++ public/app/game/game-controller.js
@@ -10,12 +10,14 @@ angular.module('app')
     .then(function successCallback(response) {
       console.log(response.data);
       $scope.game = response.data;
-      if( $scope.game.nextPlayer.turn ) {
+      if( $scope.game.nextPlayer && $scope.game.nextPlayer.turn ) {
         $scope.status = 1; // turn in progress
         $scope.playerMessage = "Now playing";
-      } else {
+      } else if ( $scope.game.nextPlayer ) {
         $scope.status = 0; // turn not in progress
         $scope.playerMessage = "Next up";
+      } else {
+        $scope.game.nextPlayer = null;
       }
     }, function errorCallback(response) {
       console.log(response);
@@ -23,6 +25,20 @@ angular.module('app')
   };
 
   $scope.loadGame();
+
+  // Increment score when an attempt is added
+  gameService.socket.on('attempt added', function(data) {
+    $scope.$applyAsync(function() {
+      console.log(data);
+      if ( data.correct ) {
+        if ( data.team === 'teamA' ) {
+          $scope.game.teamA.score += 1;
+        } else {
+          $scope.game.teamB.score += 1;
+        }
+      }
+    });
+  });
 
   // Update view when the turn ends
   gameService.socket.on('turn ended', function(data) {
--- public/app/player/play-controller.js
+++ public/app/player/play-controller.js
@@ -2,35 +2,45 @@ angular.module('app')
   .controller('PlayCtrl', ['$scope', '$http', '$cookies', '$stateParams', '$location', 'playerService', '$timeout', function ($scope, $http, $cookies, $stateParams, $location, playerService, $timeout) {
 
   $scope.player = playerService.player;
-  $scope.status = 0;
 
   playerService.setAuthHeader();
   playerService.joinRooms();
 
+  $scope.continue = function() {
+    $scope.gameStatus = 0; //Show gameplay controls.
+  }
+
+  $scope.getPlayerDetails = function() {
+    $http.get('/api/player')
+    .then(function successCallback(response) {
+      $scope.status = response.data.status;
+      $scope.gameStatus = response.data.game.status;
+      if ( response.data.turn ) {
+        $scope.turn = response.data.turn;
+        $scope.timeRemaining = $scope.turn.timeRemaining;
+        $scope.timer = $timeout($scope.onTimerTimeout, 1000);
+      }
+      console.log(response.data);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  }
+
   $scope.startTurn = function() {
     $http.post('/api/turns')
     .then(function successCallback(response) {
-      console.log(response.data);
       $scope.turn = response.data;
       $scope.status = 2;
-      $scope.timeRemaining = $scope.turnDuration();
+      $scope.timeRemaining = $scope.turn.turnDuration;
       $scope.timer = $timeout($scope.onTimerTimeout, 1000);
     }, function errorCallback(response) {
       console.log(response);
     });
   };
 
-  $scope.turnDuration = function() {
-    return Math.ceil(
-      moment.duration(moment($scope.turn.expiresAt)
-      .diff($scope.turn.createdAt)).asSeconds()
-    );
-  }
-
   $scope.onTimerTimeout = function() {
     if( $scope.timeRemaining == 0) {
       $timeout.cancel($scope.timer);
-      $scope.status = 3;
     } else {
       $scope.timeRemaining--;
       timerTimeout = $timeout($scope.onTimerTimeout, 1000);
@@ -60,16 +70,64 @@ angular.module('app')
     });
   });
 
+  // Monitor when a turn starts - mainly needed to know whether the 'turn ended' notification should be ignored.
+  playerService.socket.on('turn started', function(data) {
+    $scope.$applyAsync(function() {
+      $scope.currentTurn = data;
+    });
+  });
+
   // Update view when the turn ends, if it is now your turn.
   playerService.socket.on('turn ended', function(data) {
     console.log(data);
-    console.log($scope.player._id);
+    console.log( $scope.currentTurn._id === data.lastTurn._id );
+
+    // Check whether this notification corresponds to an active turn - it may be fired off after the round ends.
+    if ( $scope.currentTurn._id === data.lastTurn._id ) {
+      $scope.$applyAsync(function () {
+        if ( $scope.turn ) {
+          $scope.status = 3;
+          $scope.turn = null;
+
+          // Reset to status 0 if the player's view is still in the 'finished' state.
+          $timeout(function() {
+            if ( $scope.status == 3 ) {
+              $scope.status = 0;
+            }
+          }, 5000);
+
+        } else if ( data.nextPlayer._id === $scope.player._id ) {
+          $scope.status = 1;
+        }
+      });
+    }
+  });
+
+  // Update view when the turn ends, if it is now your turn.
+  playerService.socket.on('round ended', function(data) {
+    console.log(data);
     $scope.$applyAsync(function () {
-      var nextPlayer = data;
-      if ( nextPlayer._id == $scope.player._id ) {
-        $scope.status = 1;
+      // Clear out the current turn so that a subsequent notification for that turn doesn't have an effect.
+      $scope.currentTurn = null;
+
+      if( $scope.turn ) {
+        // Clear out the current player's turn.
+        $scope.turn = null;
+        $scope.status = 3;
       }
+
+      if( data === "roundOne" ) {
+        $scope.gameStatus = 2; // Show round 2 instructions
+      } else if ( data === "roundTwo" ) {
+        $scope.gameStatus = 3; // Show round 3 instructions
+      } else {
+        $scope.gameStatus = 4; // Show end of game summary
+      }
+
     });
   });
+
+  $scope.getPlayerDetails();
+
 
 }]);
